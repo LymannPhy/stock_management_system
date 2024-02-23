65de895f7d22ef6b1933167e0a7faf5253ed5082
lymann <phplaravel576@gmail.com>
2024-02-23 09:28:44 +0700

    mvc architecture

---
+++ src/model/Product.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.util.Date;
+
+public class Product {
+    private Integer code;
+    private String name;
+    private Double price;
+    private Integer qty;
+    private Date imported_at;
+
+}
